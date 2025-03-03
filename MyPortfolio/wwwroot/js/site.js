37827d68576f222b0f5b28ff88363c35e3fb7358
MohammedBagowabair <159595929+MohammedBagowabair@users.noreply.github.com>
2025-03-03 11:58:24 +0300

    Responsive NavBar

---
+++ MyPortfolio/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿window.getWindowWidth = () => {
+    return window.innerWidth;
+};
+
+window.registerResizeCallback = (dotNetHelper) => {
+    window.addEventListener("resize", () => {
+        dotNetHelper.invokeMethodAsync("OnResize", window.innerWidth);
+    });
+};
